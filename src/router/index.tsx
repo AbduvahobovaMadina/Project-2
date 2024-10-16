32219aef5d5278488aa261f6b32302c726d91682
AbduvahobovaMadina <mailto:madinaabduvahobova565@gmail.com>
2024-10-16 09:59:53 +0500

    s

---
+++ src/router/index.tsx
@@ -0,0 +1,74 @@
+import { lazy, LazyExoticComponent } from "react";
+import { useRoutes } from "react-router-dom";
+import { SuspenseComponent as Suspense } from "../utils";
+
+const Home: LazyExoticComponent<any> = lazy(() => import("../pages/home/Home"));
+const SingUp: LazyExoticComponent<any> = lazy(
+  () => import("../pages/auth/sinp-up/Sing-up")
+);
+const Layout: LazyExoticComponent<any> = lazy(
+  () => import("../pages/layout/Layout")
+);
+
+const Login: LazyExoticComponent<any> = lazy(
+  () => import("../pages/auth/login/Login")
+);
+const Auth: LazyExoticComponent<any> = lazy(() => import("../pages/auth/Auth"));
+const Routers = () => {
+  return useRoutes([
+    {
+      path: "/",
+      element: (
+        <Suspense>
+          <Layout />
+        </Suspense>
+      ),
+      children: [
+        {
+          path: "/",
+          element: (
+            <Suspense>
+              <Home />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: (
+        <Suspense>
+          <h2>404</h2>
+        </Suspense>
+      ),
+    },
+    {
+      path: "/auth",
+      element: (
+        <Suspense>
+          <Auth />
+        </Suspense>
+      ),
+      children: [
+        {
+          path: "login",
+          element: (
+            <Suspense>
+              <Login />
+            </Suspense>
+          ),
+        },
+        {
+          path: "singUp",
+          element: (
+            <Suspense>
+              <SingUp />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ]);
+};
+
+export default Routers;
